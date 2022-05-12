76e7853d0122af35297210f6b1bef9586b1b9140
zaid ragab <zragab@lcs.on.ca>
2022-05-12 15:20:00 -0400

    Done

--- Playground.playground/Pages/Empty Canvas 1.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 1.xcplaygroundpage/Contents.swift
@@ -9,7 +9,7 @@
  
  Set the size of your desired canvas by adjusting the constants on lines 10 and 11.
  */
-let preferredWidth = 500
+let preferredWidth = 650
 let preferredHeight = 500
 /*:
  ## Required code
@@ -95,5 +95,62 @@ func drawPuzzlePiece(){
 
 }
 
-drawPuzzlePiece()
+func drawRow() {
+    for _ in 1...3 {
+        t.currentPosition()
+        t.currentHeading()
+        drawPuzzlePiece()
+        t.currentPosition()
+        t.currentHeading()
 
+        // get into posstion to draw next shape
+        t.penUp()
+        t.forward(steps: 10 * scale)
+        t.penDown()
+    }
+
+}
+
+
+
+func getIntoPOstionFirstRow(){
+    t.currentPosition()
+    t.currentHeading()
+    t.currentPosition()
+    t.currentHeading()
+    t.left(by: 180)
+    t.penUp()
+    t.forward(steps: 30 * scale)
+    t.right(by: 90)
+    t.forward(steps: 5 * scale)
+    t.right(by: 90)
+    t.forward(steps: 5 * scale)
+    t.penDown()
+}
+
+
+func getIntoPostionsSecondRow(){
+    t.penUp()
+    t.left(by: 180)
+    t.forward(steps: 35 * scale)
+    t.right(by: 90)
+    t.forward(steps: 5  * scale)
+    t.right(by: 90)
+    t.currentPosition()
+    t.currentHeading()
+}
+
+
+func fillPageWithSquares(){
+    drawRow()
+    getIntoPOstionFirstRow()
+    drawRow()
+    getIntoPostionsSecondRow()
+    drawRow()
+    getIntoPOstionFirstRow()
+    drawRow()
+    getIntoPostionsSecondRow()
+    drawRow()
+}
+
+fillPageWithSquares()
